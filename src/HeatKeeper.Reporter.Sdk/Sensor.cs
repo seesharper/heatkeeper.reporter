96588a77eb4b1b8af7149671b48b6e1f1f9bb44a
Bernhard Richter <bernhard.richter@gmail.com>
2023-04-04 00:14:33 +0200

    Added sensor prefix

--- src/HeatKeeper.Reporter.Sdk/Sensor.cs
+++ src/HeatKeeper.Reporter.Sdk/Sensor.cs
@@ -2,23 +2,25 @@ namespace HeatKeeper.Reporter.Sdk
 {
     public class Sensor
     {
-        public Sensor(string model, int protocolId, Mapping[] mappings)
+        public Sensor(string model, int protocolId, Mapping[] mappings, string idPrefix)
         {
             Model = model;
             ProtocolId = protocolId;
             Mappings = mappings;
+            Prefix = idPrefix;
         }
 
         public string Model { get; }
 
         public int ProtocolId { get; }
         public Mapping[] Mappings { get; }
+        public string Prefix { get; }
     }
 
     public static class Sensors
     {
-        public static Sensor Acurite606TX => new Sensor("Acurite-606TX", 55, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("battery_ok", MeasurementType.BatteryLevel, (v) => v * 100) });
-        public static Sensor AcuriteTower => new Sensor("Acurite-Tower", 40, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("humidity", MeasurementType.Humidity, d => d), new Mapping("battery_ok", MeasurementType.BatteryLevel, (v) => v * 100) });
-        public static Sensor FineOffsetWH2 => new Sensor("Fineoffset-WH2", 18, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("humidity", MeasurementType.Humidity, d => d) });
+        public static Sensor Acurite606TX(string prefix) => new Sensor("Acurite-606TX", 55, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("battery_ok", MeasurementType.BatteryLevel, (v) => v * 100) }, prefix);
+        public static Sensor AcuriteTower(string prefix) => new Sensor("Acurite-Tower", 40, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("humidity", MeasurementType.Humidity, d => d), new Mapping("battery_ok", MeasurementType.BatteryLevel, (v) => v * 100) }, prefix);
+        public static Sensor FineOffsetWH2(string prefix) => new Sensor("Fineoffset-WH2", 18, new Mapping[] { new Mapping("temperature_C", MeasurementType.Temperature, d => d), new Mapping("humidity", MeasurementType.Humidity, d => d) }, prefix);
     }
 }
